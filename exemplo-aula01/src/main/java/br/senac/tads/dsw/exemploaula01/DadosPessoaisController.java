9d113327af94b98a7e425a3d57b919a601cd5ef3
ftsuda-senac <ftsuda.senac@gmail.com>
2024-02-19 21:10:47 -0300

    Alteração Controller para receber parâmetros

--- exemplo-aula01/src/main/java/br/senac/tads/dsw/exemploaula01/DadosPessoaisController.java
+++ exemplo-aula01/src/main/java/br/senac/tads/dsw/exemploaula01/DadosPessoaisController.java
@@ -4,11 +4,13 @@
  */
 package br.senac.tads.dsw.exemploaula01;
 
+import jakarta.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Exemplo dinâmico com Spring MVC + Thymeleaf
@@ -28,6 +30,28 @@ public class DadosPessoaisController {
         
         model.addAttribute("dados", dados);
         model.addAttribute("dataHora", LocalDateTime.now());
+        return "dados-template";
+    }
+
+    @GetMapping("/exemplo-dinamico-parametros")
+    public String mostrarTelaParametros(
+            @RequestParam("nome") String nome, 
+            @RequestParam("email") String email,
+            @RequestParam("dataNascimento") String dataNascimento,
+            @RequestParam("telefone") String telefone,
+            HttpServletRequest req,
+            Model model) {
+        DadosPessoais dados = new DadosPessoais();
+        dados.setNome(nome);
+        dados.setDataNascimento(LocalDate.parse(dataNascimento));
+        dados.setEmail(email);
+        dados.setTelefone(telefone);
+        
+        LocalDateTime dataHoraAcesso = LocalDateTime.now();
+        System.out.println(nome + "," + req.getRemoteAddr() + " - " + dataHoraAcesso);
+        
+        model.addAttribute("dados", dados);
+        model.addAttribute("dataHora", dataHoraAcesso);
         return "dados-template";
     }
     
