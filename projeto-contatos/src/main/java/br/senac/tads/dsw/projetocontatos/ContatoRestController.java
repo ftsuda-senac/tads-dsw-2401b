c0b665104903a2c4a9aea6754e6254949dfe34f4
ftsuda-senac <ftsuda.senac@gmail.com>
2024-04-08 21:38:43 -0300

    Versão básica CRUD contatos

--- projeto-contatos/src/main/java/br/senac/tads/dsw/projetocontatos/ContatoRestController.java
+++ projeto-contatos/src/main/java/br/senac/tads/dsw/projetocontatos/ContatoRestController.java
@@ -8,8 +8,13 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +33,27 @@ public class ContatoRestController {
     @GetMapping("/consulta/{id}")
     public Contato consultar(@PathVariable("id") int id) {
         return service.findById(id);
+    }
+    
+    
+    @PostMapping("/incluir")
+    public ResponseEntity<?> incluir(@RequestBody Contato c) {
+        service.save(c);
+        return ResponseEntity.ok().build();
+    }
+    
+    @PutMapping("/alterar/{id}")
+    public ResponseEntity<?> alterar(@PathVariable("id") int id,
+            @RequestBody Contato c) {
+        c.setId(id); // Ignora o ID informado no JSON - ID da URL tem predominancia
+        service.save(c);
+        return ResponseEntity.ok().build();
+    }
+    
+    @DeleteMapping("/excluir/{id}")
+    public ResponseEntity<?> excluir(@PathVariable("id") int id) {
+        service.deleteById(id);
+        return ResponseEntity.ok().build();
     }
     
 }
--- projeto-contatos/src/main/java/br/senac/tads/dsw/projetocontatos/ContatoService.java
+++ projeto-contatos/src/main/java/br/senac/tads/dsw/projetocontatos/ContatoService.java
@@ -14,17 +14,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class ContatoService {
     
+    private static int sequenciaId = 0;
+    
     private Map<Integer, Contato> contatos;
     
     public ContatoService() {
         contatos = new LinkedHashMap<>();
-        contatos.put(1, new Contato(1, "Fulano da Silva", 
+        int id = ++sequenciaId;
+        contatos.put(id, new Contato(id, "Fulano da Silva", 
                 "fulano@email.com.br", "(11) 98765-1234", 
                 LocalDate.parse("2000-10-20")));
-        contatos.put(2, new Contato(2, "Ciclano de Souza", 
+        id = ++sequenciaId;
+        contatos.put(id, new Contato(id, "Ciclano de Souza", 
                 "ciclano@email.com.br", "(11) 99001-3344", 
                 LocalDate.parse("1999-05-16")));
-        contatos.put(3, new Contato(3, "Beltrana dos Santos", 
+        id = ++sequenciaId;
+        contatos.put(id, new Contato(id, "Beltrana dos Santos", 
                 "beltrana@email.com.br", "(11) 91028-5432", 
                 LocalDate.parse("2001-07-04")));
     }
@@ -36,5 +41,21 @@ public class ContatoService {
     public Contato findById(int id) {
         return contatos.get(id);
     }
+    
+    public void save(Contato c) {
+        if (c.getId() == null) {
+            // Considera inclusão de novo contato
+            int id = ++sequenciaId;
+            c.setId(id);
+            contatos.put(id, c);
+        } else {
+            // Se ID existir, substitui dados do contato existente
+            contatos.put(c.getId(), c);
+        }
+    }
+    
+   public void deleteById(int id) {
+       contatos.remove(id);
+   }
     
 }
