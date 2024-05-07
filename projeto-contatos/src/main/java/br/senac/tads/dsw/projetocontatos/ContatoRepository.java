9c86b4a0b28d48e02bb8e6e20e59a7d07350a8e6
ftsuda-senac <ftsuda.senac@gmail.com>
2024-05-06 22:44:52 -0300

    Exemplos de métodos de busca customizados

--- projeto-contatos/src/main/java/br/senac/tads/dsw/projetocontatos/ContatoRepository.java
+++ projeto-contatos/src/main/java/br/senac/tads/dsw/projetocontatos/ContatoRepository.java
@@ -4,9 +4,22 @@
  */
 package br.senac.tads.dsw.projetocontatos;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
-public interface ContatoRepository extends JpaRepository<Contato, Integer> {
+public interface ContatoRepository extends 
+        JpaRepository<Contato, Integer> {
     
+    Optional<Contato> findByNome(String nome);
+    
+    Optional<Contato> findByEmail(String email);
+    
+    List<Contato> findByDataNascimentoGreaterThanEqual(LocalDate data);
+    
+    @Query(nativeQuery = true, value = "SELECT * FROM contato WHERE id > ?1")
+    List<Contato> obterIdsMaioresQueX(int x);
 }
